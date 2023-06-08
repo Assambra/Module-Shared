24c3243eecc26f09c750384ba02040c21d10f224
Philipp Leutner <philipp.leutner@assambra.com>
2023-06-08 23:49:26 +0200

    PlayerController: Gravity and Jump

--- Shared/Assets/Assambra/Game Framework/Modules/Shared/Scripts/Controller/PlayerController.cs
+++ Shared/Assets/Assambra/Game Framework/Modules/Shared/Scripts/Controller/PlayerController.cs
@@ -14,8 +14,15 @@ public class PlayerController : MonoBehaviour
     [Header("Rotation speed")]
     [SerializeField] private float rotationSpeed = 150f;
 
+    [Header("Jumping")]
+    [SerializeField] private float jumpHeight = 2.0f;
+
     // Private variables
     private Vector3 movement = Vector3.zero;
+
+    private bool isGrounded = false;
+    private Vector3 playerVelocity;
+    private float gravity = Physics.gravity.y;
 
     private void Awake()
     {
@@ -28,6 +35,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         GetAxisInput();
+
+        if (playerVelocity.y < 0f && isGrounded)
+            playerVelocity.y = gravity * Time.deltaTime;
+        else
+            playerVelocity.y += gravity * Time.deltaTime;
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+
+        playerVelocity.y += gravity * Time.deltaTime;
+        characterController.Move(playerVelocity * Time.deltaTime);
+        
+        isGrounded = characterController.isGrounded;
     }
 
     private void FixedUpdate()
